2488074e97a0cb69f186d7cd24e36fa1e046a659
RifkiPermana <rfkiprmna5@gmail.com>
2025-06-24 16:44:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: "/rifki-portofolio",
+  plugins: [react(), tailwindcss()],
 })
