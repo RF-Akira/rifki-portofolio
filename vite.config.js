34cb71498ffc190596143ef0c5e47a4772ad978d
RF-Akira <rfkiprmna5@gmail.com>
2025-06-24 18:38:49 +0700

    fix: final correction for image path and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/rifki-portofolio",
+  base: "/rifki-portofolio/",
   plugins: [react(), tailwindcss()],
 })
